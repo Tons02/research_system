ff836ac005a568f2d6fa4af26025f29a230e0534
Antonio Montilla <system.montilla@gmail.com>
2024-12-04 16:45:47 +0800

    add change password and reset password

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CompanyController;
@@ -11,22 +12,28 @@ use App\Http\Controllers\Api\LocationController;
 use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-// ymir coa 
-Route::resource("companies", CompanyController::class);
-Route::resource("business-unit", BusinessUnitController::class);
-Route::resource("departments", DepartmentController::class);
-Route::resource("units", UnitController::class);
-Route::resource("sub-units", SubUnitController::class);
-Route::resource("locations", LocationController::class);
+Route::post('login', [AuthController::class, 'login']);
 
-//Role Controller
-Route::put('role-archived/{id}', [RoleController::class, 'archived']);
-Route::resource("role", RoleController::class);
+Route::middleware(['auth:sanctum'])->group(function () {
+    // ymir coa 
+    Route::resource("companies", CompanyController::class)->middleware(['abilities:masterlist:sync']);
+    Route::resource("business-unit", BusinessUnitController::class)->middleware(['abilities:masterlist:sync']);
+    Route::resource("departments", DepartmentController::class)->middleware(['abilities:masterlist:sync']);
+    Route::resource("units", UnitController::class)->middleware(['abilities:masterlist:sync']);
+    Route::resource("sub-units", SubUnitController::class)->middleware(['abilities:masterlist:sync']);
+    Route::resource("locations", LocationController::class)->middleware(['abilities:masterlist:sync']);
 
-//User Controller
-Route::put('user-archived/{id}', [UserController::class, 'archived']);
-Route::resource("user", UserController::class);
+    //Role Controller
+    Route::put('role-archived/{id}', [RoleController::class, 'archived'])->middleware(['abilities:role-management:crud']);
+    Route::resource("role", RoleController::class)->middleware(['abilities:role-management:crud']);
+
+    //User Controller
+    Route::put('user-archived/{id}', [UserController::class, 'archived'])->middleware(['abilities:user-management:crud']);
+    Route::resource("user", UserController::class)->middleware(['abilities:user-management:crud']);
+
+    // auth controller 
+    Route::patch('changepassword', [AuthController::class, 'changedPassword']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::patch('resetpassword/{id}', [AuthController::class, 'resetPassword']);
+});
